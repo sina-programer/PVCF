94d03e3ba6bdc156cbb81366971271d57ac28a87
Sina <sina.programerr@gmail.com>
2024-01-22 17:55:33 +0330

    Prevent closing immediately

--- main.py
+++ main.py
@@ -137,3 +137,4 @@ if __name__ == '__main__':
 
     print(f'The contacts were exported in <{OUTPUT_PATH}> successfully!')
     print('Added Contacts:', len(contacts))
+    input('Press <enter> to exit...')
